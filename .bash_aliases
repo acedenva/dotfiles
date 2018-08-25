1d4735b1bc9d33161b880095cefa0e6a33c16802
noanthro <ailurophobisch@gmail.com>
2018-08-25 11:40:08 +0200

    improvements to secrets

--- .bash_aliases
+++ .bash_aliases
@@ -2,8 +2,8 @@ if [ -f ${HOME}/.secrets.bash ]; then
 	source ${HOME}/.secrets.bash
 fi
 
-alias ssh-refra="ssh -i ${HOME}/.ssh/id_rsa -p ${S_portR} ${S_userR}@${S_serverR}"
-alias ssh-refra:steam="ssh -i ${HOME}/.ssh/id_rsa -p ${S_portR} steam@${S_serverR}"
+alias ssh-refra="ssh -i ${HOME}/.ssh/id_rsa -p ${Sec_portRef} ${Sec_userRef}@${Sec_serverRef}"
+alias ssh-refra-steam="ssh -i ${HOME}/.ssh/id_rsa -p ${Sec_portRef} steam@${Sec_serverRef}"
 alias gid="git --work-tree=${HOME} --git-dir=${HOME}/dotfiles"
 alias tmux-basic="\
 	tmux new-session -d -s basic
--- .init.bash
+++ .init.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
 #
+if [ ! -d ${HOME}/.vim/bundle/Vundle.vim ]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
+fi
+
+cloud='/mnt/d/Cloud'
+if [ -d ${cloud} ]; then
+	rsync -r -u ${cloud}/crypt/unix/.ssh ${HOME}/
+	ln -s ${cloud}/drive/scripts ${HOME}/scripts
+	ln -s ${cloud}/crypt/unix/.secrets.bash ${HOME}/.secrets.bash
+	chmod -R 500 ${HOME}/.ssh ${HOME}/scripts ${HOME}/.secrets.bash
+fi
+
 if [ ! -f ${HOME}/.secrets.bash ]; then
 	# Template .secrets.bash -- maybe with sed
 	# export S_userR=''
@@ -8,10 +20,4 @@ if [ ! -f ${HOME}/.secrets.bash ]; then
 	echo 'no .secrets.bash'	
 fi
 
-if [ ! -d ${HOME}/.vim/bundle/Vundle.vim ]; then
-	git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
-fi
 
-if [ -d /mnt/d/Cloud/drive/scripts ]; then
-	ln -s /mnt/d/Cloud/drive/scripts ${HOME}/scripts
-fi
