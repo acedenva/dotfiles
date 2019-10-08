8063ac247c65375da46129136cdef8308fc1cb4b
ailurophobisch <ailurophobisch@gmail.com>
2019-10-08 03:57:50 +0200

    new auto include for repo folder

--- init.bash
+++ init.bash
@@ -3,13 +3,18 @@
 if [ ! -d ${HOME}/.vim/bundle/Vundle.vim ]; then
 	git clone https://github.com/VundleVim/Vundle.vim.git ${HOME}/.vim/bundle/Vundle.vim
 fi
+repos='/mnt/c/Users/lobo/repos'
+if [ -d ${repos} ]; then
+	ln -s ${repos} ${HOME}
+fi
 
 cloud='/mnt/d/Cloud'
 if [ -d ${cloud} ]; then
 	rsync -r -u ${cloud}/crypt/unix/.ssh ${HOME}/
 	ln -s ${cloud}/crypt/unix/.secrets.bash ${HOME}/.secrets.bash
 	ln -s ${cloud}/drive/scripts ${HOME}/scripts
-	chmod -R 500 ${HOME}/.ssh ${HOME}/scripts ${HOME}/.secrets.bash
+	chmod -R 500 ${HOME}/.ssh ${HOME}/.secrets.bash
+	chmod -R 770 ${HOME}/scripts 
 fi
 
 if [ ! -f ${HOME}/.secrets.bash ]; then
