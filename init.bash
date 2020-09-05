9335a90bb50758556f30d15b983a90a337bd8b08
noanthro <ailurophobisch@gmail.com>
2020-09-06 01:59:46 +0200

    keys exchange

--- init.bash
+++ init.bash
@@ -5,19 +5,19 @@ if [ ! -d ${HOME}/.vim/bundle/Vundle.vim ]; then
 fi
 # creating symlinks in home for WSL+Seafile  
 if [[ -x $(which powershell.exe) ]]; then
-	winuser=$(powershell.exe -Command '[ENVIRONMENT]::GetEnvironmentVariable("Username")' | sed -r 's/(([a-z]|[0-9])+)/\1/ig')
-	hostname=$(powershell.exe -Command '[ENVIRONMENT]::GetEnvironmentVariable("Computername")' | sed -r s/\n//g)
-	echo ${winuser}
+	winuser=$(powershell.exe -Command '[ENVIRONMENT]::GetEnvironmentVariable("Username")'\
+		| sed -r 's/\s+//g')
+	hostname=$(powershell.exe -Command '[ENVIRONMENT]::GetEnvironmentVariable("Computername")'\
+		| sed -r 's/\s+//g')
 	sdrive="/mnt/c/Users/${winuser}/Seafile/sdrive"
-	###rchive="/mnt/c/Users/${winuser}/Seafile/rchive"
-
-	echo ${sdrive}
+	rchive="/mnt/c/Users/${winuser}/Seafile/rchive"
 	if [[ -d ${sdrive} ]]; then
-		echo ${drive}
-		exit
-		rsync -rutv ${sdrive}/unix/.ssh ${HOME}/
-		rsync -rutv ${HOME}/.ssh ${sdrive}/unix/
-		ln -s ${sdrive}/unix/.secrets.bash ${HOME}/.secrets.bash
+		if [[ ! -d ${sdrive}/keys/${hostname} ]]; then
+			mkdir -p ${sdrive}/keys/${hostname}
+		fi
+		rsync -rutv ${HOME}/.ssh ${sdrive}/keys/${hostname}/
+		rsync -rutv ${sdrive}/keys/${hostname}/.ssh ${HOME}/
+		ln -s ${sdrive}/keys/.secrets.bash ${HOME}/.secrets.bash
 		ln -s ${sdrive}/drive/scripts ${HOME}/scripts
 		chmod -R 700 ${HOME}/.ssh ${HOME}/.secrets.bash
 		chmod -R 770 ${HOME}/scripts 
